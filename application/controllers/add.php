1502a804cf7504ba1ad5ef82aabe324601a0caf1
onehandharry <androidloggedin@gmail.com>
2013-11-18 15:38:53 +0000

    Removed duplicate ); line 10

--- application/controllers/add.php
+++ application/controllers/add.php
@@ -5,13 +5,13 @@ class Add extends CI_Controller {
 	public function index()
 	{
 
-		$cow_name = $this->input->post('name'); 
-		$owner = $this->input->post('owner');		
-		$this->load->model('stock/add_cow'););	
+		$cow_name = $this->input->post('name');
+		$owner = $this->input->post('owner');
+		$this->load->model('stock/add_cow');
 		$this->add_cow->add_a_cow($cow_name, $owner);
-		
+
 	}
-	
+
 }
 
 /* End of file add.php */
